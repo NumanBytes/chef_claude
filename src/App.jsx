8aacbb8b3a1cdc56df790e13923221e2c0db73d0
Numan <numanrazzaq350@gmail.com>
2025-02-12 04:52:45 +0500

    listing mapped

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import './App.css';
-import Header from './container/Header';
-import MainForm from './container/Main';
+import "./App.css";
+import Header from "./container/Header";
+import MainForm from "./container/Main";
 
 function App() {
   return (
--- src/container/Main/index.jsx
+++ src/container/Main/index.jsx
@@ -1,16 +1,36 @@
-import './styles.css'
+import React, { useState } from 'react';
+import "./styles.css";
 
 const MainForm = () => {
-    return (
-        <form className="main-form">
-            <input 
-                type="text"
-                placeholder="e.g, Oregeon"
-                aria-label="Add Ingredient"
-            />
-            <button>Add Ingredient</button>
-        </form>
-    )
-}
+  const [IngredientList, setIngredientList] = useState([])
+  const [inputValue, setInputValue] = useState('')
+  
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (inputValue.trim() === "") return;
+    setIngredientList([...IngredientList, inputValue])
+    setInputValue("");
+  }
+
+  return (
+    <main>
+      <form className="main-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="text"
+          placeholder="i.g, Oregeon"
+          value={inputValue}
+          aria-label="Add Ingredient"
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button type='submit'>Add Ingredient</button>
+      </form>
+      <ul>
+        {IngredientList.map((ingredient, index) => (<li key={index} className='listing-styles'>{ingredient}</li>))}
+      </ul>
+    </main>
+  );
+};
 
 export default MainForm;
